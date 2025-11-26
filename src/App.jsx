8bf0b6249dee2bdb723018352f6afe1a94b597d2
Benjamin Rino <benjar.rino@gmail.com>
2025-11-26 10:13:25 +0100

    fetching external API done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <></>;
+  const [trips, setTrips] = useState([""]);
+
+  useEffect(() => {
+    fetch("https://tripapi.cphbusinessapps.dk/api/trips")
+      .then((response) => response.json())
+      .then((data) => setTrips(data))
+      .catch((error) => console.error("Error fetching joke:", error));
+  }, []);
+
+  return (
+    <>
+      <h1>Trips</h1>
+      <ul>
+        {trips.map((trip) => (
+          // hele objektet vises således: <li key={trip.id}>{JSON.stringify(trip)}</li>
+          <li key={trip.id}>{trip.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
